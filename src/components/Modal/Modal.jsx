ffe13df178982dd5dd373b65ddf501017ee92249
OlegRosinskyi <olegrosinskyi@gmail.com>
2023-03-02 20:58:35 +0200

    Upodate Modal.jsx
    Upodate Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,13 +8,13 @@ import { ModalWindous } from "./Modal.stiled";
 const modulRoot = document.querySelector('#modal-root');
 
 const Modal = memo(function Modal({ onClose, children }) {
-     const handleKeydown = useCallback( (event) => {
-            if (event.code === 'Escape')
-            {
-               // console.log('{Закриття модалки Escape}');
-                onClose();
-            }
-        } ,[onClose])    
+    const handleKeydown = useCallback((event) => {
+        if (event.code === 'Escape') {
+            // console.log('{Закриття модалки Escape}');
+            onClose();
+        }
+    }, [onClose]);
+    
       const  handleBackdropClick = event => {
             if (event.target === event.currentTarget) {
                 //console.log('{Click in Backdrop}');
@@ -26,7 +26,7 @@ const Modal = memo(function Modal({ onClose, children }) {
     useEffect(() => {
         window.addEventListener('keydown', handleKeydown); return () => {
         window.removeEventListener('keydown', handleKeydown);
-    };},[])
+    };},[handleKeydown])
 
          return createPortal(<>
                 <Backdrop onClick={handleBackdropClick}>
